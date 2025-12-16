a463022b098c5eb8d484c184cd94f2315718f36e
Andrew Besedin <andrewprog97@gmail.com>
2025-12-16 23:22:31 +0300

    add: add eslint & prettier updated config

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    // Or 'jejolare/backend' for Node.js app
+    extends: ['jejolare/frontend'],
+
+    // This is needed only if you use TypeScript
+    settings: {
+        'import/resolver': {
+            typescript: {
+                project: './tsconfig.json',
+            },
+        },
+    },
+    rules: {
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
+    },
+};
